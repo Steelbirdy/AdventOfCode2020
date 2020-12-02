a447babf32d055bd04e65acdd0261f2c96ae3aff
Steelbirdy <patrickammons@comcast.net>
2020-12-02 00:19:20 -0500

    Finished day 2; timed @ 6:00 minutes / 13:03 minutes

---
+++ day2/__init__.py
@@ -0,0 +1,45 @@
+from utils import read_input
+
+
+def convert_password_part1(pw: str) -> tuple:
+    pw = pw.split(' ', maxsplit=3)
+    a, b = pw[0].split('-')
+    return (
+        range(int(a), int(b)+1),
+        pw[1][0],
+        pw[2],
+    )
+
+
+def convert_password_part2(pw: str) -> tuple:
+    pw = pw.split(' ', maxsplit=3)
+    a, b = pw[0].split('-')
+    return (
+        (int(a), int(b)),
+        pw[1][0],
+        pw[2],
+    )
+
+
+def is_valid_part1(pw: tuple):
+    a, b, c = pw
+    a = set(a)
+    return sum(1 if k == b else 0 for k in c) in a
+
+
+def is_valid_part2(pw: tuple):
+    a, b, c = pw
+    m, M = a
+    return (c[m-1] == b) != (c[M-1] == b)
+
+
+if __name__ == '__main__':
+    data = read_input('input.txt', by_line=True)
+
+    # Part 1
+    passwords = [convert_password_part1(x) for x in data]
+    print(sum(1 if is_valid_part1(pw) else 0 for pw in passwords))
+
+    # Part 2
+    passwords = [convert_password_part2(x) for x in data]
+    print(sum(1 if is_valid_part2(pw) else 0 for pw in passwords))
